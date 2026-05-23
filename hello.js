00cc5b3e3b62782d7f1ac3a29ac738a65d750589
ikartik00 <kartikrathore50600@gmail.com>
2026-05-23 09:45:18 +0530

    solved

--- hello.js
+++ hello.js
@@ -1,12 +1,7 @@
 const sayHelllo = ()=>{
     console.log("Hello");
-<<<<<<< Updated upstream
     console.log("Hello5");
-}
-=======
     console.log("Hello12");
     console.log("Hello12");
-    
-    
 }
->>>>>>> Stashed changes
+
