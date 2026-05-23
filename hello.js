4ab2f421f1032cfb39a8c97a1d451663fa2730b1
ikartik00 <kartikrathore50600@gmail.com>
2026-05-23 09:41:22 +0530

    hello2

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
 const sayHelllo = ()=>{
     console.log("Hello");
+<<<<<<< Updated upstream
     console.log("Hello5");
 }
+=======
+    console.log("Hello12");
+    console.log("Hello12");
+    
+    
+}
+>>>>>>> Stashed changes
